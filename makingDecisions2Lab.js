7f199e94b23821dde396b8674b84fbb2b132ad15
Jessica Steen <jessicalsteen@yahoo.com>
2021-10-14 15:15:15 -0600

    commit 4

--- makingDecisions2Lab.js
+++ makingDecisions2Lab.js
@@ -41,11 +41,17 @@ let evensArr = []
 // Do not edit the code above.
 
 /*
-  Use a for-loop to iterate through each number in the 'nums' array, if the number is even, append it to the 'evensArr'.
+  Use a for-loop to iterate through each number in the 'nums' array, if the number is 
+  even, append it to the 'evensArr'.
 */
 
 //Code Here
-
+ 
+for (var i = 0; i <nums.length; i++) {
+  if(nums[i] % 2 === 0) {
+    evensArr.push(nums[i]) }
+  }
+  console.log(evensArr)
 
 
 ////////// PROBLEM 5 //////////
